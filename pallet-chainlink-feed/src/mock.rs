f1439395c8a41b6e0e2e5286b02ccf7281e10e96
clearloop <udtrokia@gmail.com>
2021-05-12 18:08:34 +0800

    feat(feed): ensure mutating storage safely with #[require_transactional]

--- pallet-chainlink-feed/src/mock.rs
+++ pallet-chainlink-feed/src/mock.rs
@@ -219,3 +219,25 @@ pub fn new_test_ext() -> sp_io::TestExternalities {
 
 	t.into()
 }
+
+#[macro_export]
+macro_rules! tx_assert_ok {
+	($e:expr) => {
+		with_transaction_result(|| -> Result<(), ()> {
+			assert_ok!($e);
+			Ok(())
+		})
+		.unwrap();
+	};
+}
+
+// #[macro_export]
+// macro_rules! tx_assert_noop {
+// 	($l:expr, $r:expr) => {
+// 		with_transaction_result(|| -> Result<(), ()> {
+// 			assert_noop!($l, $r);
+// 			Ok(())
+// 		})
+// 		.unwrap();
+// 	};
+// }
