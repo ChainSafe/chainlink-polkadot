c2379e6c99386f8e587217bbd5f1e5e543eecffe
clearloop <udtrokia@gmail.com>
2021-05-20 16:47:17 +0800

    chore(feed): remove function count_overflow

--- pallet-chainlink-feed/src/lib.rs
+++ pallet-chainlink-feed/src/lib.rs
@@ -701,8 +701,7 @@ pub mod pallet {
 		/// Updates the pruning window of an existing feed
 		///
 		/// - Will prune rounds if the given window is smaller than the existing one.
-		#[pallet::weight(T::WeightInfo::set_pruning_window(
-            <Feed<T>>::count_overflow_windows(*feed_id, *pruning_window)))]
+		#[pallet::weight(T::WeightInfo::set_pruning_window(Feed::<T>::load_from(*feed_id).map(|feed|feed.current_window()).unwrap_or_default().saturating_sub(*pruning_window)))]
 		pub fn set_pruning_window(
 			origin: OriginFor<T>,
 			feed_id: T::FeedId,
@@ -1264,7 +1263,6 @@ pub mod pallet {
 	/// Proxy used for interaction with a feed.
 	/// `should_sync` flag determines whether the `config` is put into
 	/// storage on `drop`.
-	#[derive(Default)]
 	pub struct Feed<T: Config> {
 		pub id: T::FeedId,
 		pub config: FeedConfigOf<T>,
@@ -1305,26 +1303,6 @@ pub mod pallet {
 				config,
 				should_sync: true,
 			})
-		}
-
-		/// Wrapping `current_window` outside this struct
-		///
-		/// NOTE:
-		///
-		/// This function is only for determining weights for
-		/// extrinsic `set_pruning_window`
-		pub fn count_overflow_windows(id: T::FeedId, pruning_window: RoundId) -> u32 {
-			if let Some(config) = Feeds::<T>::get(id) {
-				Self {
-					id,
-					config,
-					should_sync: false,
-				}
-				.current_window()
-			} else {
-				0
-			}
-			.saturating_sub(pruning_window) as u32
 		}
 
 		// --- getters ---
